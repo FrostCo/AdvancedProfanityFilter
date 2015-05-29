f6c49b9c0f710de74823d85ed1b972dde68d8b45
Richard Frost <richard@richardfrost.info>
2015-05-28 18:02:44 -0600

    Upgrade to Manifest v2, Cleanup Code, HTTPS Support

--- filter.js
+++ filter.js
@@ -13,31 +13,31 @@ port.postMessage({localStorage: "preserveFirst"});
 port.postMessage({localStorage: "filterSubstring"});
 port.onMessage.addListener(function(msg) {
   if (msg.wordList) {
-	wordList = msg.wordList.split(",");
-	if (readyPreserveFirst && readyFilterSubstring) {
-		// When all local storage retrieved, begin removing profanity
-		generateProfanityList();
-		removeProfanity();
-	}
-	readyWordList = true;
+  wordList = msg.wordList.split(",");
+  if (readyPreserveFirst && readyFilterSubstring) {
+    // When all local storage retrieved, begin removing profanity
+    generateProfanityList();
+    removeProfanity();
+  }
+  readyWordList = true;
   }
   if (msg.preserveFirst) {
-	preserveFirst = (msg.preserveFirst == "true");
-	if (readyWordList && readyFilterSubstring) {
-		// When all local storage retrieved, begin removing profanity
-		generateProfanityList();
-		removeProfanity();
-	}
-	readyPreserveFirst = true;
+  preserveFirst = (msg.preserveFirst == "true");
+  if (readyWordList && readyFilterSubstring) {
+    // When all local storage retrieved, begin removing profanity
+    generateProfanityList();
+    removeProfanity();
+  }
+  readyPreserveFirst = true;
   }
   if (msg.filterSubstring) {
-	filterSubstring = (msg.filterSubstring == "true");
-	if (readyWordList && readyPreserveFirst) {
-		// When all local storage retrieved, begin removing profanity
-		generateProfanityList();
-		removeProfanity();
-	}
-	readyFilterSubstring = true;
+  filterSubstring = (msg.filterSubstring == "true");
+  if (readyWordList && readyPreserveFirst) {
+    // When all local storage retrieved, begin removing profanity
+    generateProfanityList();
+    removeProfanity();
+  }
+  readyFilterSubstring = true;
   }
 });
 
@@ -46,68 +46,68 @@ document.addEventListener('DOMNodeInserted', removeProfanityFromNode, false);
 
 // Parse the profanity list
 function generateProfanityList() {
-	if (filterSubstring) {
-		for (var x = 0; x < wordList.length; x++) {
-			profanityList.push(new RegExp("(" + wordList[x][0] + ")" + wordList[x].substring(1), "gi" ));
-		}
-	} else {
-		for (var x = 0; x < wordList.length; x++) {
-			profanityList.push(new RegExp("\\b(" + wordList[x][0] + ")" + wordList[x].substring(1) + "\\b", "gi" ));
-		}
-	}
+  if (filterSubstring) {
+    for (var x = 0; x < wordList.length; x++) {
+      profanityList.push(new RegExp("(" + wordList[x][0] + ")" + wordList[x].substring(1), "gi" ));
+    }
+  } else {
+    for (var x = 0; x < wordList.length; x++) {
+      profanityList.push(new RegExp("\\b(" + wordList[x][0] + ")" + wordList[x].substring(1) + "\\b", "gi" ));
+    }
+  }
 }
 
 // Remove the profanity from the document
 function removeProfanity() {
-	var evalResult = document.evaluate(
-		'.//text()[normalize-space(.) != ""]',
-		document,
-		null,
-		XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-		null
-	);
-	
-	for (var i = 0; i < evalResult.snapshotLength; i++) {
-		var textNode = evalResult.snapshotItem(i);
-		for (var z = 0; z < profanityList.length; z++) {
-			textNode.data = textNode.data.replace(profanityList[z], starReplace);
-		}
-	}
+  var evalResult = document.evaluate(
+    './/text()[normalize-space(.) != ""]',
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+  
+  for (var i = 0; i < evalResult.snapshotLength; i++) {
+    var textNode = evalResult.snapshotItem(i);
+    for (var z = 0; z < profanityList.length; z++) {
+      textNode.data = textNode.data.replace(profanityList[z], starReplace);
+    }
+  }
 }
 
 // Remove the profanity from the node
 function removeProfanityFromNode(event) {
-	var node = event.target;
-	
-	var evalResult = document.evaluate(
-		'.//text()[normalize-space(.) != ""]',
-		node,
-		null,
-		XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-		null
-	);
-	
-	for (var i = 0; i < evalResult.snapshotLength; i++) {
-		var textNode = evalResult.snapshotItem(i);
-		for (var z = 0; z < profanityList.length; z++) {
-			textNode.data = textNode.data.replace(profanityList[z], starReplace);
-		}
-	}
+  var node = event.target;
+  
+  var evalResult = document.evaluate(
+    './/text()[normalize-space(.) != ""]',
+    node,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+  
+  for (var i = 0; i < evalResult.snapshotLength; i++) {
+    var textNode = evalResult.snapshotItem(i);
+    for (var z = 0; z < profanityList.length; z++) {
+      textNode.data = textNode.data.replace(profanityList[z], starReplace);
+    }
+  }
 }
 
 // Replace the profanity with a string of asterisks
 function starReplace(strMatchingString, strFirstLetter) {
-	var starString = "";
-	if (!preserveFirst) {
-		for (var i = 0; i < strMatchingString.length; i++) {
-			starString = starString + "*";
-		}
-	} else {
-		starString = strFirstLetter;
-		for (var i = 1; i < strMatchingString.length; i++) {
-			starString = starString + "*";
-		}
-	}
-	
-	return starString;
+  var starString = "";
+  if (!preserveFirst) {
+    for (var i = 0; i < strMatchingString.length; i++) {
+      starString = starString + "*";
+    }
+  } else {
+    starString = strFirstLetter;
+    for (var i = 1; i < strMatchingString.length; i++) {
+      starString = starString + "*";
+    }
+  }
+  
+  return starString;
 }
--- options.js
+++ options.js
@@ -1,40 +1,39 @@
-var website = "https://chrome.google.com/extensions/detail/ackkocjhcalcpgpfjcoinogdejibgbho";
+// Add event listeners to DOM
+window.addEventListener("load", restore_options);
+document.getElementById("toggleProfanity").addEventListener("click", toggleProfanity);
+document.getElementById("save").addEventListener("click", save_options);
+document.getElementById("close").addEventListener("click", function(){window.close();});
 
 // Saves options to localStorage.
 function save_options() {
-	var listbox = document.getElementById("wordList");
-	var wordList = listbox.value;
-	localStorage["wordList"] = wordList;
-	
-	if (document.myForm.preserveFirst.checked) {
-		localStorage["preserveFirst"] = "true";
-	} else {
-		localStorage["preserveFirst"] = "false";
-	}
-	
-	if (document.myForm.filterSubstring.checked) {
-		localStorage["filterSubstring"] = "true";
-	} else {
-		localStorage["filterSubstring"] = "false";
-	}
+  var listbox = document.getElementById("wordList");
+  var wordList = listbox.value;
+  localStorage.wordList = wordList;
+  
+  if (document.myForm.preserveFirst.checked) {
+    localStorage.preserveFirst = "true";
+  } else {
+    localStorage.preserveFirst = "false";
+  }
+  
+  if (document.myForm.filterSubstring.checked) {
+    localStorage.filterSubstring = "true";
+  } else {
+    localStorage.filterSubstring = "false";
+  }
 }
 
 // Restores form state to saved values from localStorage.
 function restore_options() {
-	var listFavorite = localStorage["wordList"];
-	if (!listFavorite) {
-		return;
-	}
-	var listbox = document.getElementById("wordList");
-	listbox.value = listFavorite;
-	
-	document.myForm.preserveFirst.checked = (localStorage["preserveFirst"] == "true");
-	document.myForm.filterSubstring.checked = (localStorage["filterSubstring"] == "true");
-}
-
-// Opens the official website in a new tab
-function openWebsite() {
-	chrome.tabs.create({url: website});
+  var listFavorite = localStorage.wordList;
+  if (!listFavorite) {
+    return;
+  }
+  var listbox = document.getElementById("wordList");
+  listbox.value = listFavorite;
+  
+  document.myForm.preserveFirst.checked = (localStorage.preserveFirst == "true");
+  document.myForm.filterSubstring.checked = (localStorage.filterSubstring == "true");
 }
 
 // Displays the profanity list and hides the profanity button
