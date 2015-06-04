46912871da0055b9a22cf5e12d7bcad68fd05a05
Richard Frost <richard@richardfrost.info>
2015-06-03 21:24:06 -0600

    Add code for detecting extension install/upgrade

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,13 @@
 // Open options page if extension icon is clicked
 chrome.browserAction.onClicked.addListener(function() {chrome.runtime.openOptionsPage();});
 
-// Set defaults on installation
-chrome.runtime.onInstalled.addListener(function(){});
+// // Actions for extension install or upgrade
+// chrome.runtime.onInstalled.addListener(function(details){
+//   if (details.reason == "install"){
+//     console.log('first install');
+//     chrome.runtime.openOptionsPage();
+//   } else if (details.reason == "update") {
+//     var thisVersion = chrome.runtime.getManifest().version;
+//     console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+//   }
+// });
