9783aa648c6efce3ed9e66945a47fc414cdd3111
Richard Frost <richard.frost@mx.com>
2017-08-15 14:02:03 -0600

    Alternate ways to create filter

---
+++ filter1.js
@@ -0,0 +1,125 @@
+var wordList, preserveFirst, filterSubstring, showCounter;
+var wordRegExps = [];
+var defaults = {'wordList': 'asshole,bastard,bitch,cunt,damn,fuck,piss,slut,shit,tits,whore', 'preserveFirst': false, 'filterSubstring': true, 'showCounter': true};
+var counter = 0;
+var xpathDocText = '//*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
+var xpathNodeText = './/*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
+
+function checkNodeForProfanity(mutation) {
+  mutation.addedNodes.forEach(function(node) {
+    if (!isForbiddenNode(node)) {
+      removeProfanity(xpathNodeText, node);
+    }
+  });
+}
+
+function cleanPage() {
+  chrome.storage.sync.get(defaults, function(storage) {
+    // Load settings and setup environment
+    wordList = storage.wordList.split(',');
+    filterSubstring = storage.filterSubstring;
+    preserveFirst = storage.preserveFirst;
+    showCounter = storage.showCounter;
+    generateRegexpList();
+
+    // Remove profanity from the main document and watch for new nodes
+    removeProfanity(xpathDocText);
+  });
+  observeNewNodes();
+}
+
+// Parse the profanity list
+function generateRegexpList() {
+  if (filterSubstring) {
+    for (var x = 0; x < wordList.length; x++) {
+      wordRegExps.push(new RegExp('(' + wordList[x][0] + ')' + wordList[x].substring(1), 'gi' ));
+    }
+  } else {
+    for (var x = 0; x < wordList.length; x++) {
+      wordRegExps.push(new RegExp('\\b(' + wordList[x][0] + ')' + wordList[x].substring(1) + '\\b', 'gi' ));
+    }
+  }
+}
+
+// Returns true if a node should *not* be altered in any way
+// Credit: https://github.com/ericwbailey/millennials-to-snake-people/blob/master/Source/content_script.js
+function isForbiddenNode(node) {
+  return node.isContentEditable || // DraftJS and many others
+  (node.parentNode && node.parentNode.isContentEditable) || // Special case for Gmail
+  (node.tagName && (node.tagName.toLowerCase() == "textarea" || // Some catch-alls
+                    node.tagName.toLowerCase() == "input" ||
+                    node.tagName.toLowerCase() == "script" ||
+                    node.tagName.toLowerCase() == "style")
+  );
+}
+
+// Watch for new text nodes and clean them as they are added
+function observeNewNodes() {
+  var observerConfig = {
+    childList: true,
+    subtree: true
+  };
+
+  // When DOM is modified, remove profanity from inserted node
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      checkNodeForProfanity(mutation);
+    });
+  });
+
+  // Remove profanity from new objects
+  observer.observe(document, observerConfig);
+}
+
+function removeProfanity(xpathExpression, node = document) {
+  var evalResult = document.evaluate(
+    xpathExpression,
+    node,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+
+  for (var i = 0; i < evalResult.snapshotLength; i++) {
+    var textNode = evalResult.snapshotItem(i);
+    textNode.data = replaceText(textNode.data);
+  }
+}
+
+function replaceText(str) {
+  for (var z = 0; z < wordList.length; z++) {
+    str = str.replace(wordRegExps[z], starReplace);
+  }
+
+  return str;
+}
+
+// Replace the profanity with a string of asterisks
+// Only gets run when there is a match in replaceText()
+function starReplace(strMatchingString, strFirstLetter) {
+  var starString = '';
+
+  if (!preserveFirst) {
+    for (var i = 0; i < strMatchingString.length; i++) {
+      starString = starString + '*';
+    }
+  } else {
+    starString = strFirstLetter;
+    for (var i = 1; i < strMatchingString.length; i++) {
+      starString = starString + '*';
+    }
+  }
+
+  updateCounter();
+  return starString;
+}
+
+// Updates the counter and displays it if enabled
+function updateCounter() {
+  if (showCounter) {
+    counter++;
+    chrome.runtime.sendMessage({counter: counter.toString()});
+  }
+}
+
+cleanPage();
