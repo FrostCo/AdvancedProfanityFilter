a5e06199c45f9423bdf1474ff072f2947f44d338
Richard Frost <richard.frost@mx.com>
2017-11-21 11:51:44 -0700

    Update icon when disabled

--- filter.js
+++ filter.js
@@ -1,6 +1,6 @@
 var wordList, preserveFirst, filterSubstring, showCounter;
 var wordRegExps = [];
-var defaults = {'wordList': 'asshole,bastard,bitch,cunt,damn,fuck,piss,slut,shit,tits,whore', 'preserveFirst': false, 'filterSubstring': true, 'showCounter': true, 'disabledDomains': ''};
+var defaults = {'disabledDomains': '', 'filterSubstring': true, 'preserveFirst': false, 'showCounter': true, 'wordList': 'asshole,bastard,bitch,cunt,damn,fuck,piss,slut,shit,tits,whore'};
 var counter = 0;
 var xpathDocText = '//*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
 var xpathNodeText = './/*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
@@ -25,7 +25,10 @@ function cleanPage() {
     disabledDomains = storage.disabledDomains.split(',');
 
     // Remove profanity from the main document and watch for new nodes
-    if (disabledPage()) { return false; }
+    if (disabledPage()) {
+      chrome.runtime.sendMessage({disabled: true});
+      return false;
+    }
     generateRegexpList();
     removeProfanity(xpathDocText);
     updateCounterBadge();
@@ -38,12 +41,15 @@ function disabledPage() {
   domain = window.location.hostname;
 
   for (var x = 0; x < disabledDomains.length; x++) {
-    console.log("Checking domain: ", disabledDomains[x]);
-    domainRegex = new RegExp("(^|\.)" + disabledDomains[x]);
-    if (domainRegex.test(domain)) {
-      console.log("Blocked domain!");
-      disabled = true;
-      break;
+    if (disabledDomains[x]) {
+      console.log(disabledDomains);
+      console.log("Checking domain: ", disabledDomains[x]);
+      domainRegex = new RegExp("(^|\.)" + disabledDomains[x]);
+      if (domainRegex.test(domain)) {
+        console.log("Blocked domain!");
+        disabled = true;
+        break;
+      }
     }
   }
 
