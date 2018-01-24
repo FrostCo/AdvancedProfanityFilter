45729a7d36500956cd9ad4b25892cc5a903e2e5c
Richard Frost <richard.frost@mx.com>
2018-01-24 11:36:07 -0700

    Add support for global and per-word pattern matching

--- filter.js
+++ filter.js
@@ -1,32 +1,52 @@
 var counter = 0;
+// matchMethod = ["exact", "partial", "whole"]
 var defaults = {
   "censorCharacter": "*",
   "censorFixedLength": 0,
   "censorRemoveWord": false,
   "disabledDomains": [],
   "filterMethod": 0, // ["censor", "substitute"];
-  "matchMethod": 1, // ["exact", "partial", "greedy"]
+  "globalMatchMethod": 3, // ["exact", "partial", "whole", "disabled"]
   "preserveFirst": false,
   "showCounter": true,
   "words": {
-    "asshole": ["butthole", "jerk"],
-    "bastard": ["imperfect", "impure"],
-    "bitch": ["jerk"],
-    "cunt": ["explative"],
-    "damn": ["dang", "darn"],
-    "fuck": ["freak", "fudge"],
-    "piss": ["pee"],
-    "pissed": ["ticked"],
-    "slut": ["imperfect", "impure"],
-    "shit": ["crap", "crud", "poop"],
-    "tits": ["explative"],
-    "whore": ["harlot", "tramp"]
+    "ass": {"matchMethod": 0, "words": ["butt", "tail"] },
+    "asshole": {"matchMethod": 1, "words": ["butthole", "jerk"] },
+    "bastard": {"matchMethod": 1, "words": ["imperfect", "impure"] },
+    "bitch": {"matchMethod": 1, "words": ["jerk"] },
+    "cunt": {"matchMethod": 1, "words": ["explative"] },
+    "damn": {"matchMethod": 1, "words": ["dang", "darn"] },
+    "fuck": {"matchMethod": 1, "words": ["freak", "fudge"] },
+    "piss": {"matchMethod": 1, "words": ["pee"] },
+    "pissed": {"matchMethod": 0, "words": ["ticked"] },
+    "slut": {"matchMethod": 1, "words": ["imperfect", "impure"] },
+    "shit": {"matchMethod": 1, "words": ["crap", "crud", "poop"] },
+    "tits": {"matchMethod": 1, "words": ["explative"] },
+    "whore": {"matchMethod": 1, "words": ["harlot", "tramp"] }
   }
 };
-var censorCharacter, censorFixedLength, censorRemoveWord, disabledDomains, filterMethod, matchMethod, preserveFirst, showCounter, substitutionWords, words;
+var censorCharacter, censorFixedLength, censorRemoveWord, disabledDomains, filterMethod, globalMatchMethod, matchMethod, preserveFirst, showCounter, substitutionWords, words;
 var wordRegExps = [];
 var xpathDocText = '//*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
 var xpathNodeText = './/*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
+
+// Word must match exactly (not sub-string)
+// /\b(w)ord\b/gi
+function build_exact_regexp(word) {
+  wordRegExps.push(new RegExp('\\b(' + word[0] + ')' + word.substring(1) + '\\b', 'gi' ));
+}
+
+// Match any part of a word (sub-string)
+// /(w)ord/gi
+function build_part_regexp(word) {
+  wordRegExps.push(new RegExp('(' + word[0] + ')' + word.substring(1), 'gi' ));
+}
+
+// Match entire word that contains sub-string
+// /\b[\w-]*(w)ord[\w-]*\b/gi
+function build_whole_regexp(word) {
+  wordRegExps.push(new RegExp('\\b([\\w-]*' + word[0] + ')' + word.substring(1) + '[\\w-]*\\b', 'gi' ));
+}
 
 function checkNodeForProfanity(mutation) {
   mutation.addedNodes.forEach(function(node) {
@@ -72,6 +92,7 @@ function cleanPage() {
     censorRemoveWord = storage.censorRemoveWord
     disabledDomains = storage.disabledDomains;
     filterMethod = storage.filterMethod;
+    globalMatchMethod = storage.globalMatchMethod;
     matchMethod = storage.matchMethod;
     preserveFirst = storage.preserveFirst;
     showCounter = storage.showCounter;
@@ -113,27 +134,37 @@ function disabledPage() {
 }
 
 // Parse the profanity list
+// ["exact", "partial", "whole", "disabled"]
 function generateRegexpList() {
-  switch(matchMethod) {
-    case 0:
-      // Word must match exactly (not sub-string)
-      // /\b(w)ord\b/gi
+  switch(globalMatchMethod) {
+    case 0: // Global: Exact match
       for (var x = 0; x < words.length; x++) {
-        wordRegExps.push(new RegExp('\\b(' + words[x][0] + ')' + words[x].substring(1) + '\\b', 'gi' ));
+        build_exact_regexp(words[x]);
       }
       break;
-    case 1:
-      // Match any part of a word (sub-string)
-      // /(w)ord/gi
+    case 2: // Global: Whole word match
       for (var x = 0; x < words.length; x++) {
-        wordRegExps.push(new RegExp('(' + words[x][0] + ')' + words[x].substring(1), 'gi' ));
+        build_whole_regexp(words[x]);
       }
       break;
-    case 2:
-      // Match entire word that contains sub-string
-      // /\b[\w-]*(w)ord[\w-]*\b/gi
+    case 3: // Per-word matching
       for (var x = 0; x < words.length; x++) {
-        wordRegExps.push(new RegExp('\\b([\\w-]*' + words[x][0] + ')' + words[x].substring(1) + '[\\w-]*\\b', 'gi' ));
+        switch(substitutionWords[words[x]].matchMethod) {
+          case 0: // Exact match
+            build_exact_regexp(words[x]);
+            break;
+          case 2: // Whole word match
+            build_whole_regexp(words[x]);
+            break;
+          default: // case 1 - Partial word match (Default)
+            build_part_regexp(words[x]);
+            break;
+        }
+      }
+      break;
+    default: // case 1 - Global: Partial word match (Default)
+      for (var x = 0; x < words.length; x++) {
+        build_part_regexp(words[x]);
       }
       break;
   }
