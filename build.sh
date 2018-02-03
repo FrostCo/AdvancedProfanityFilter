52361c3157b30f67376380079fb0de3e0c54d08c
Richard Frost <richard.frost@mx.com>
2018-02-02 22:36:54 -0700

    Enhance build script

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+branch_name=$(git symbolic-ref --short -q HEAD)
+
 if [ "$(uname -s)" == "Darwin" ]; then
   # MacOS
-  zip -vr "./extension.zip" ./ -i eventPage.js filter.js icons/\* manifest.json options.\* -x .git
+  if [ $branch_name == "master" ]; then
+    zip -vr "./extension-chrome.zip" ./ -i eventPage.js filter.js icons/\* manifest.json options.\* -x .git
+    cp ./extension-{chrome.zip,opera.nex}
+  elif [ $branch_name == "firefox" ]; then
+    zip -vr "./extension-firefox.zip" ./ -i eventPage.js filter.js icons/\* manifest.json options.\* -x .git
+  fi
 else
   # Linux
-  7za u -tzip ./extension.zip -aoa -ir\!eventPage.js filter.js icons/* manifest.json options.* -xr\!.git
+  if [ $branch_name == "master" ]; then
+    7za u -tzip "./extension-chrome.zip" -aoa -ir\!eventPage.js filter.js icons/* manifest.json options.* -xr\!.git
+    cp ./extension-{chrome.zip,opera.nex}
+  elif [ $branch_name == "firefox" ]; then
+    7za u -tzip "./extension-firefox.zip" -aoa -ir\!eventPage.js filter.js icons/* manifest.json options.* -xr\!.git
+  fi
 fi
