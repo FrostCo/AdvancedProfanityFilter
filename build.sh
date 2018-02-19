b3b7fd753ba3bff691c875bcee9a0d8ed8d79182
Richard Frost <richard.frost@mx.com>
2018-02-19 13:28:40 -0700

    Fix popup

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ branch_name=$(git symbolic-ref --short -q HEAD)
 if [ "$(uname -s)" == "Darwin" ]; then
   # MacOS
   if [ $branch_name == "master" ]; then
-    zip -vr "./extension-chrome.zip" ./ -i eventPage.js filter.js icons/\* manifest.json options.\* -x .git
+    zip -vr "./extension-chrome.zip" ./ -i eventPage.js filter.js icons/\* manifest.json options.\* popup.\* -x .git
     cp ./extension-{chrome.zip,opera.nex}
   elif [ $branch_name == "firefox" ]; then
-    zip -vr "./extension-firefox.zip" ./ -i eventPage.js filter.js icons/\* manifest.json options.\* -x .git
+    zip -vr "./extension-firefox.zip" ./ -i eventPage.js filter.js icons/\* manifest.json options.\* popup.\* -x .git
   fi
 else
   # Linux
   if [ $branch_name == "master" ]; then
-    7za u -tzip "./extension-chrome.zip" -aoa -ir\!eventPage.js filter.js icons/* manifest.json options.* -xr\!.git
+    7za u -tzip "./extension-chrome.zip" -aoa -ir\!eventPage.js filter.js icons/* manifest.json options.* popup.* -xr\!.git
     cp ./extension-{chrome.zip,opera.nex}
   elif [ $branch_name == "firefox" ]; then
-    7za u -tzip "./extension-firefox.zip" -aoa -ir\!eventPage.js filter.js icons/* manifest.json options.* -xr\!.git
+    7za u -tzip "./extension-firefox.zip" -aoa -ir\!eventPage.js filter.js icons/* manifest.json options.* popup.* -xr\!.git
   fi
 fi
