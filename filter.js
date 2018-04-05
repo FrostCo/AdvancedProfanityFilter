ff4470e1abde7e93df736cc78a755d5a6a11528f
Richard Frost <richard.frost@mx.com>
2018-04-05 11:41:01 -0600

    Improve isForbiddenNode

--- filter.js
+++ filter.js
@@ -223,19 +223,26 @@ function generateRegexpList() {
 // Returns true if a node should *not* be altered in any way
 // Credit: https://github.com/ericwbailey/millennials-to-snake-people/blob/master/Source/content_script.js
 function isForbiddenNode(node) {
-  return Boolean(node.isContentEditable || // DraftJS and many others
-  (node.parentNode && node.parentNode.isContentEditable) || // Special case for Gmail
-  (node.parentNode && (node.parentNode.tagName == "SCRIPT" ||
-                       node.parentNode.tagName == "STYLE"
+  return Boolean(
+    node.isContentEditable || // DraftJS and many others
+    (node.parentNode && (
+                          node.parentNode.isContentEditable || // Special case for Gmail
+                          node.parentNode.tagName == "SCRIPT" ||
+                          node.parentNode.tagName == "STYLE" ||
+                          node.parentNode.tagName == "INPUT" ||
+                          node.parentNode.tagName == "TEXTAREA" ||
+                          node.parentNode.tagName == "IFRAME"
+                        )
+    ) || // Some catch-alls
+    (node.tagName &&  (
+                        node.tagName == "SCRIPT" ||
+                        node.tagName == "STYLE" ||
+                        node.tagName == "INPUT" ||
+                        node.tagName == "TEXTAREA" ||
+                        node.tagName == "IFRAME"
                       )
-  ) || // Some catch-alls
-  (node.tagName && (node.tagName == "IFRAME" ||
-                    node.tagName == "INPUT" ||
-                    node.tagName == "TEXTAREA" ||
-                    node.tagName == "SCRIPT" ||
-                    node.tagName == "STYLE"
-                   )
-  ));
+    )
+  );
 }
 
 // Watch for new text nodes and clean them as they are added
