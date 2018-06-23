01c9cc6de11e7597287eb8addb224395aecd9540
Richard Frost <richardfrost@users.noreply.github.com>
2018-06-23 11:04:34 -0600

    move options to OptionPage

--- build.sh
+++ build.sh
@@ -25,6 +25,6 @@ else
 fi
 
 tsc --outfile ./dist/eventPage.js ./src/helper.ts ./src/config.ts ./src/eventPage.ts --target es6
-tsc --outfile ./dist/filter.js ./src/helper.ts ./src/config.ts ./src/filter.ts --target es6
-tsc --outfile ./dist/options.js ./src/helper.ts ./src/config.ts ./src/options.ts --target es6
+tsc --outfile ./dist/filter.js ./src/helper.ts ./src/config.ts ./src/word.ts ./src/page.ts ./src/filter.ts --target es6
+tsc --outfile ./dist/optionPage.js ./src/helper.ts ./src/config.ts ./src/optionAuth.ts ./src/optionTab.ts ./src/optionPage.ts --target es6
 tsc --outfile ./dist/popup.js ./src/helper.ts ./src/config.ts ./src/domain.ts ./src/popup.ts --target es6
