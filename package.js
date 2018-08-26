a1c0a921b9e561b56e35c99e9353d49249346432
Richard Frost <richardfrost@users.noreply.github.com>
2018-08-26 17:36:40 -0600

    Update package.js process to persist version

--- package.js
+++ package.js
@@ -2,50 +2,51 @@
 
 const fs = require('fs');
 const path = require('path');
-var AdmZip = require('adm-zip');
+const AdmZip = require('adm-zip');
 
-var zip = new AdmZip();
-var dist = './dist/'
+function buildChromeExtension(zip) {
+  // try { fs.unlinkSync('./dist/filter.js'); } catch {}; // Remove filter.js as its only used for testing
+  zip.deleteFile('filter.js'); // Remove filter.js as its only used for testing
+  zip.writeZip('./extension-chrome.zip');
+}
 
-function updateManifestFile(zip, obj) {
-  var content = JSON.stringify(obj, null, 2);
+// Firefox Extension
+function buildFirefox(manifest, zip) {
+  let firefoxManifest = {
+    "applications": {
+      "gecko": {
+        "id": "{853d1586-e2ab-4387-a7fd-1f7f894d2651}"
+      }
+    }
+  };
+  manifest.applications = firefoxManifest.applications;
+  updateManifestFileInZip(zip, manifest);
+  zip.writeZip('./extension-firefox.zip');
+}
+
+function updateManifestFile(file, obj) {
+  let content = JSON.stringify(obj, null, 2);
+  fs.writeFileSync(file, content);
+}
+
+function updateManifestFileInZip(zip, obj) {
+  let content = JSON.stringify(obj, null, 2);
   zip.updateFile('manifest.json', Buffer.alloc(content.length, content));
 }
 
-// Chrome Extension
-// try { fs.unlinkSync('./dist/filter.js'); } catch {};
-zip.addLocalFolder(dist, null)
-var manifest = JSON.parse(fs.readFileSync(path.join(dist, 'manifest.json')));
-manifest.version = process.env.npm_package_version || manifest.version;
-updateManifestFile(zip, manifest);
-zip.deleteFile('filter.js'); // Remove filter.js as its only used for testing. Use filter.bundle.js instead
-zip.writeZip('./extension-chrome.zip');
-
-// Firefox Extension
-var firefoxManifest = {
-  "applications": {
-    "gecko": {
-      "id": "{853d1586-e2ab-4387-a7fd-1f7f894d2651}"
-    }
+function updateManifestVersion(manifestPath, manifest) {
+  if (manifest.version != process.env.npm_package_version) {
+    console.log('Version number is being updated: ' + manifest.version + ' -> ' + process.env.npm_package_version)
+    manifest.version = process.env.npm_package_version || manifest.version;
+    updateManifestFile(manifestPath, manifest);
   }
-};
-manifest.applications = firefoxManifest.applications;
-updateManifestFile(zip, manifest);
-zip.writeZip('./extension-firefox.zip');
+}
 
-// function walkSync(dir, filelist = []) {
-//   fs.readdirSync(dir).forEach(file => {
-//     const dirFile = path.join(dir, file);
-//     try {
-//         filelist = walkSync(dirFile, filelist);
-//     }
-//     catch (err) {
-//         if (err.code === 'ENOTDIR' || err.code === 'EBUSY') filelist = [...filelist, dirFile];
-//         else throw err;
-//     }
-//   });
-//   return filelist;
-// }
-
-// var files = walkSync(dist);
-// files.forEach(file => { zip.addLocalFile(file); });
+const dist = './dist/'
+let zip = new AdmZip();
+let manifestPath = path.join(dist, 'manifest.json');
+let manifest = JSON.parse(fs.readFileSync(manifestPath));
+updateManifestVersion(manifestPath, manifest);
+zip.addLocalFolder(dist, null);
+buildChromeExtension(zip);
+buildFirefox(manifest, zip);
