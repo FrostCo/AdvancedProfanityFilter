8a77979134a3c0f0e00f2cec1c1de0fc130642e5
Richard Frost <richardfrost@users.noreply.github.com>
2018-09-24 14:42:30 -0600

    Fix packaging script and package source

--- tools/package-extension.js
+++ tools/package-extension.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const fs = require('fs');
+const fse = require('fs-extra');
 const path = require('path');
 const AdmZip = require('adm-zip');
 
 function buildChrome(zip) {
+  fse.removeSync('./extension-chrome.zip');
   console.log('Building ./extension-chrome.zip');
-  // try { fs.unlinkSync('./dist/filter.js'); } catch {}; // Remove filter.js as its only used for testing
   zip.deleteFile('webFilter.js'); // Remove filter.js as its only used for testing
-  zip.writeZip(path.join(dist, './extension-chrome.zip'));
+  zip.writeZip('./extension-chrome.zip');
 }
 
 function buildFirefox(manifest, zip) {
+  fse.removeSync('./extension-firefox.zip');
   console.log('Building ./extension-firefox.zip');
   let firefoxManifest = {
     "applications": {
@@ -22,19 +23,18 @@ function buildFirefox(manifest, zip) {
   };
   manifest.applications = firefoxManifest.applications;
   updateManifestFileInZip(zip, manifest);
-  zip.writeZip(path.join(dist, './extension-firefox.zip'));
+  zip.writeZip('./extension-firefox.zip');
 }
 
 function buildOpera(zip) {
+  fse.removeSync('./extension-opera.zip');
   console.log('Building ./extension-opera.zip');
-  // try { fs.unlinkSync('./dist/filter.js'); } catch {}; // Remove filter.js as its only used for testing
-  zip.deleteFile('webFilter.js'); // Remove filter.js as its only used for testing
-  zip.writeZip(path.join(dist, './extension-opera.zip'));
+  zip.writeZip('./extension-opera.zip');
 }
 
 function updateManifestFile(file, obj) {
   let content = JSON.stringify(obj, null, 2);
-  fs.writeFileSync(file, content);
+  fse.writeFileSync(file, content);
 }
 
 function updateManifestFileInZip(zip, obj) {
@@ -47,17 +47,42 @@ function updateManifestVersion(manifestPath, manifest) {
     console.log('Version number is being updated: ' + manifest.version + ' -> ' + process.env.npm_package_version)
     manifest.version = process.env.npm_package_version || manifest.version;
     updateManifestFile(manifestPath, manifest);
-    fs.copyFileSync(manifestPath, path.join(dist, 'manifest.json'));
+    fse.copyFileSync(manifestPath, path.join(dist, 'manifest.json'));
   }
+}
+
+function packageSource() {
+  fse.removeSync('./extension-source.zip');
+  console.log('Building ./extension-source.zip');
+  console.log('Build from source: npm install && npm run build && npm run package-no-build');
+
+  let sourceZip = new AdmZip();
+  let files = [
+    '.eslintrc.json',
+    'LICENSE',
+    'package.json',
+    'package-lock.json',
+    'README.md',
+    'tsconfig.json'
+  ];
+  sourceZip.addLocalFolder('./src', 'src');
+  sourceZip.addLocalFolder('./static', 'static');
+  sourceZip.addLocalFolder('./test', 'test');
+  sourceZip.addLocalFolder('./tools', 'tools');
+  files.forEach(file => { sourceZip.addLocalFile(path.join('./', file), null)});
+  sourceZip.writeZip('./extension-source.zip');
 }
 
 const dist = './dist/'
 const staticDir = './static/'
 let zip = new AdmZip();
 let manifestPath = path.join(staticDir, 'manifest.json');
-let manifest = JSON.parse(fs.readFileSync(manifestPath));
+let manifest = JSON.parse(fse.readFileSync(manifestPath));
 updateManifestVersion(manifestPath, manifest);
 zip.addLocalFolder(dist, null);
 buildChrome(zip);
 buildOpera(zip);
 buildFirefox(manifest, zip);
+
+// Required for Firefox
+packageSource();
