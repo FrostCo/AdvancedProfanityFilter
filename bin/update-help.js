fa0eb1ae702854c72deebe10ded2401c2d0836b7
Richard Frost <richardfrost@users.noreply.github.com>
2018-10-05 22:08:23 -0600

    Update help docs

--- bin/update-help.js
+++ bin/update-help.js
@@ -8,17 +8,17 @@ const download = require('download');
 let readmeURI = 'https://raw.githubusercontent.com/wiki/richardfrost/AdvancedProfanityFilter/Home.md';
 let optionPage = path.join('static', 'optionPage.html');
 let optionPageHTML = fs.readFileSync(optionPage).toString();
-let prefix = '<div id="gettingStarted">';
+let prefix = '<div id="helpContainer">';
 let postfix = '\n  </div>';
 let foundMatch = false;
 
-console.log('Downloading Readme...');
+console.log('Downloading Wiki...');
 download(readmeURI).then(data => {
   console.log('Parsing markdown...');
   let md = data.toString();
   let html = marked(md);
 
-  let newOptionPageHTML = optionPageHTML.replace(/<div id="gettingStarted">.*?<\/div>/s, function(match) {
+  let newOptionPageHTML = optionPageHTML.replace(/<div id="helpContainer">.*?<\/div>/s, function(match) {
     foundMatch = true;
     let output = prefix;
     html.split('\n').forEach(line => { if (line.trim() != '') output += `\n    ${line}` });
