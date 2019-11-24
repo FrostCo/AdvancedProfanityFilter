8a8bc927a82ae5a219ccf8aee1c5a6f2dc6b2c1b
Richard Frost <richardfrost@users.noreply.github.com>
2019-11-24 15:38:50 -0700

    :art: Switch from var to let

--- src/script/eventPage.ts
+++ src/script/eventPage.ts
@@ -10,7 +10,7 @@ chrome.runtime.onInstalled.addListener(function(details){
   if (details.reason == 'install') {
     chrome.runtime.openOptionsPage();
   } else if (details.reason == 'update') {
-    // var thisVersion = chrome.runtime.getManifest().version;
+    // let thisVersion = chrome.runtime.getManifest().version;
     // console.log('Updated from ' + details.previousVersion + ' to ' + thisVersion);
 
     // Open options page to show new features
