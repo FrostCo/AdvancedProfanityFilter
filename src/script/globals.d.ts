4e16944a42c34b7cde4bb217d550bd01c7c0c938
Richard Frost <richardfrost@users.noreply.github.com>
2020-02-03 23:15:18 -0700

    :art: Alphabetize AudioRules

--- src/script/globals.d.ts
+++ src/script/globals.d.ts
@@ -1,18 +1,18 @@
 interface AudioRules {
   mode: string;                     // 'cue', 'element', 'text', 'watcher'
-  className?: string;               // [Element] node.className.includes()
   checkInterval?: number;           // [Watcher] Set a custom watch interval (in ms)
+  className?: string;               // [Element] node.className.includes()
   containsSelector?: string;        // [Element] node.querySelector() [Not commonly used]
   dataPropPresent?: string;         // [Element] node.dataset.hasOwnProperty()
   filterSubtitles?: boolean;        // [All] Filter subtitle text [Default: true]
   hasChildrenElements?: boolean;    // [Element] node.childElementCount > 0 [Not commonly used]
   iframe?: boolean | undefined;     // [All] pages to run on (true: only iframes, false: no iframes, undefined: all)
   muteMethod?: number;              // [All] Override global muteMthod (0: tab, 1: video)
+  parentSelector?: string;          // [Text] parent.contains(node)
   removeSubtitleSpacing?: boolean;  // [Element] Remove subtitle padding/margin when hiding
   showSubtitles?: number;           // [All] Override global showSubtitles (0: all, 1: filtered, 2: unfiltered, 3: none)
   subtitleSelector?: string;        // [Element,Watcher] *Used for Filtering*: node.querySelector()
   tagName?: string;                 // [Element] *REQUIRED*: node.nodeName
-  parentSelector?: string;          // [Text] parent.contains(node)
   trackProcessed?: boolean;         // [Watcher] Attempt to only process text once
   videoCueHideCues?: boolean;       // [Cue] Hide activeCues instead of textTrack.mode = 'hidden'
   videoCueLanguage?: string;        // [Cue] Language for video TextTrack
