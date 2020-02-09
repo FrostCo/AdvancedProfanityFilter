75f40e0d6bc310f3585b2c1fad7a10d698a17dd3
Richard Frost <richardfrost@users.noreply.github.com>
2020-02-09 09:41:15 -0700

    :green_heart: Add extension/ to clean script

--- bin/clean.js
+++ bin/clean.js
@@ -2,6 +2,6 @@
 
 const fse = require('fs-extra');
 
-let folders = ['./dist', './test/built', './extension-chrome', './extension-firefox']
+let folders = ['./dist', './test/built', './extension', './extension-chrome', './extension-firefox']
 
 folders.forEach((folder) => { console.log(`Cleaning ${folder}...`); fse.removeSync(folder); });
