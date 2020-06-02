d26d6a592a7ed203c3d511318004478c7975b290
Richard Frost <richardfrost@users.noreply.github.com>
2020-06-01 21:52:20 -0600

    :bug: Fix hiding hulu captions
     #216

--- src/script/webAudioSites.ts
+++ src/script/webAudioSites.ts
@@ -42,7 +42,7 @@ export default class WebAudioSites {
         tagName: 'DIV',
       }
     ],
-    'www.hulu.com': [{ mode: 'element', className: 'caption-text-box', subtitleSelector: 'p', tagName: 'DIV' }],
+    'www.hulu.com': [{ mode: 'element', className: 'caption-text-box', displaySelector: 'div.caption-text-box', subtitleSelector: 'p', tagName: 'DIV' }],
     'www.nbc.com': [
       { mode: 'element', className: 'ttr-line', subtitleSelector: 'span.ttr-cue', tagName: 'DIV' },
       { mode: 'cue', videoCueLanguage: 'en' }
