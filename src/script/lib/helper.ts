388b968df02bd6c0a0bee2c7e4e8ca9ec02c4fb7
Richard Frost <richardfrost@users.noreply.github.com>
2020-06-29 12:24:21 -0600

    :fire: Remove alternate number formatter for counter

--- src/script/lib/helper.ts
+++ src/script/lib/helper.ts
@@ -1,25 +1,3 @@
-// https://stackoverflow.com/a/57718593 - ttk
-export function abbreviateNumber(number: number, digits: number = 2): string {
-  let expK = Math.floor(Math.log10(Math.abs(number)) / 3);
-  let scaled = number / Math.pow(1000, expK);
-
-  if (Math.abs(parseFloat(scaled.toFixed(digits))) >= 1000) { // Check for rounding to next exponent
-    scaled /= 1000;
-    expK += 1;
-  }
-
-  const SI_SYMBOLS = 'apμm kMGTPE';
-  let BASE0_OFFSET = SI_SYMBOLS.indexOf(' ');
-
-  if (expK + BASE0_OFFSET>=SI_SYMBOLS.length) { // Bound check
-    expK = SI_SYMBOLS.length-1 - BASE0_OFFSET;
-    scaled = number / Math.pow(1000, expK);
-  }
-  else if (expK + BASE0_OFFSET < 0) return '';  // return 0; Too small
-
-  return scaled.toFixed(digits).replace(/(\.|(\..*?))0+$/,'$2') + SI_SYMBOLS[expK+BASE0_OFFSET].trim();
-}
-
 /* istanbul ignore next */
 export function dynamicList(list: string[], select: HTMLSelectElement, title?: string) {
   removeChildren(select);
