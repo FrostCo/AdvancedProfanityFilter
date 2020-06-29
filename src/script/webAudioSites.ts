8a7a274895a8a68e35e399a08ba2ed2669f99594
Richard Frost <richardfrost@users.noreply.github.com>
2020-06-29 12:26:37 -0600

    :mute: Add another parent selector for HBO Max
     #222

--- src/script/webAudioSites.ts
+++ src/script/webAudioSites.ts
@@ -44,7 +44,7 @@ export default class WebAudioSites {
       }
     ],
     'play.google.com': [{ className: 'lava-timed-text-window', mode: 'element', subtitleSelector: 'span.lava-timed-text-caption', tagName: 'DIV' }],
-    'play.hbomax.com': [{ mode: 'elementChild', parentSelectorAll: 'div.class3 > span', showSubtitles: 0, tagName: 'SPAN' }],
+    'play.hbomax.com': [{ mode: 'elementChild', parentSelectorAll: 'div.class3 > span, div.class28 > span', showSubtitles: 0, tagName: 'SPAN' }],
     'www.hulu.com': [{ mode: 'element', className: 'caption-text-box', displaySelector: 'div.caption-text-box', subtitleSelector: 'p', tagName: 'DIV' }],
     'www.nbc.com': [
       { mode: 'element', className: 'ttr-line', subtitleSelector: 'span.ttr-cue', tagName: 'DIV' },
