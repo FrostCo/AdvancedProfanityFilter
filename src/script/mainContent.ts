ce266adc684a108b292307474e44c568d2caffe5
Richard Frost <richard@richardfrost.info>
2023-02-12 14:41:58 -0700

    :fire: Clean up mainContent

--- src/script/mainContent.ts
+++ src/script/mainContent.ts
@@ -4,7 +4,5 @@ const filter = new WebFilter;
 
 if (typeof window !== 'undefined' && ['[object Window]', '[object ContentScriptGlobalScope]'].includes(({}).toString.call(window))) {
   filter.initPageDetails();
-
-  /* istanbul ignore next */
   filter.cleanPage();
 }
