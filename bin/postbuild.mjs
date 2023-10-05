233de8ef78f8bfecaf0ae7c709054851dde17601
Richard Frost <richard@richardfrost.info>
2023-10-04 23:54:55 -0600

    :recycle: Rewrite PostBuild script into class

--- bin/postbuild.mjs
+++ bin/postbuild.mjs
@@ -8,69 +8,78 @@ import {
   writeJSONFile
 } from './lib.mjs';
 
-let buildData;
-let manifest;
-
-function common() {
-  handleManifest();
-}
-
-function edgeLegacyBuild() {
-  const msPreload = {
-    backgroundScript: 'backgroundScriptsAPIBridge.js',
-    contentScript: 'contentScriptsAPIBridge.js'
-  };
-
-  // Fix options_page
-  manifest.options_page = manifest.options_ui.page;
-  delete manifest.options_ui;
-
-  // Add ms-proload polyfills
-  manifest['-ms-preload'] = msPreload;
-  fse.copyFileSync('./store/edge/src/backgroundScriptsAPIBridge.js', './dist/backgroundScriptsAPIBridge.js');
-  fse.copyFileSync('./store/edge/src/contentScriptsAPIBridge.js', './dist/contentScriptsAPIBridge.js');
-}
-
-function firefoxBuild() {
-  manifest.browser_specific_settings = {
-    gecko: {
-      id: '{853d1586-e2ab-4387-a7fd-1f7f894d2651}'
-    }
-  };
-
-  switch (buildData.manifestVersion) {
-    case 2: firefoxMv2Build(); break;
-    case 3: firefoxMv3Build(); break;
+export default class PostBuild {
+  constructor() {
+    this.buildData = loadJSONFile(buildFilePath);
+    this.manifest = loadJSONFile(distManifestPath);
   }
-}
 
-function firefoxMv2Build() {
-  manifest.background.persistent = true; // Event pages are not supported
-}
+  common() {
+    this.handleManifest();
+  }
 
-function firefoxMv3Build() {
-  manifest.background = {
-    scripts: ['background.js'],
-  };
-}
+  edgeLegacyBuild() {
+    const msPreload = {
+      backgroundScript: 'backgroundScriptsAPIBridge.js',
+      contentScript: 'contentScriptsAPIBridge.js'
+    };
 
-function handleManifest() {
-  if (manifest.version != buildData.version) manifest.version = buildData.version;
+    // Fix options_page
+    this.manifest.options_page = this.manifest.options_ui.page;
+    delete this.manifest.options_ui;
 
-  if (buildData.manifestVersion == 2) {
-    manifest.permissions.splice(manifest.permissions.indexOf('scripting'), 1);
-    manifest.action = undefined;
-    manifest.manifest_version = buildData.manifestVersion;
-    manifest.options_ui = {
+    // Add ms-proload polyfills
+    this.manifest['-ms-preload'] = msPreload;
+    fse.copyFileSync('./store/edge/src/backgroundScriptsAPIBridge.js', './dist/backgroundScriptsAPIBridge.js');
+    fse.copyFileSync('./store/edge/src/contentScriptsAPIBridge.js', './dist/contentScriptsAPIBridge.js');
+  }
+
+  updateManifestVersion() {
+    if (this.manifest.version != this.buildData.version) this.manifest.version = this.buildData.version;
+  }
+
+  firefoxBuild() {
+    this.manifest.browser_specific_settings = {
+      gecko: {
+        id: '{853d1586-e2ab-4387-a7fd-1f7f894d2651}'
+      }
+    };
+
+    switch (this.buildData.manifestVersion) {
+      case 2: this.firefoxMv2Build(); break;
+      case 3: this.firefoxMv3Build(); break;
+    }
+  }
+
+  firefoxMv2Build() {
+    this.manifest.background.persistent = true; // Event pages are not supported
+  }
+
+  firefoxMv3Build() {
+    this.manifest.background = {
+      scripts: ['background.js'],
+    };
+  }
+
+  handleManifest() {
+    this.updateManifestVersion();
+    this.handleManifestVersions();
+  }
+
+  handleManifestV2() {
+    this.manifest.permissions.splice(this.manifest.permissions.indexOf('scripting'), 1);
+    this.manifest.action = undefined;
+    this.manifest.manifest_version = this.buildData.manifestVersion;
+    this.manifest.options_ui = {
       chrome_style: false,
       open_in_tab: true,
       page: 'optionPage.html',
     };
-    manifest.background = {
+    this.manifest.background = {
       persistent: false,
       scripts: ['background.js'],
     };
-    manifest.browser_action = {
+    this.manifest.browser_action = {
       default_icon: {
         '19': 'img/icon19.png',
         '38': 'img/icon38.png',
@@ -79,21 +88,35 @@ function handleManifest() {
       default_title: 'Advanced Profanity Filter',
     };
   }
-}
 
-function main() {
-  buildData = loadJSONFile(buildFilePath);
-  manifest = loadJSONFile(distManifestPath);
-
-  // Perform postbuild actions
-  common();
-
-  switch (buildData.target) {
-    case 'edgeLegacy': edgeLegacyBuild(); break;
-    case 'firefox': firefoxBuild(); break;
+  handleManifestV3() {
+    // General customizations for all Manifest V3 builds
   }
 
-  writeJSONFile(distManifestPath, manifest);
+  handleManifestVersions() {
+    switch (this.buildData.manifestVersion) {
+      case 2: this.handleManifestV2(); break;
+      case 3: this.handleManifestV3(); break;
+    }
+  }
+
+  main() {
+    this.common();
+    this.targetCustomizations();
+    this.writeManifestFile();
+  }
+
+  targetCustomizations() {
+    switch (this.buildData.target) {
+      case 'edgeLegacy': this.edgeLegacyBuild(); break;
+      case 'firefox': this.firefoxBuild(); break;
+    }
+  }
+
+  writeManifestFile() {
+    writeJSONFile(distManifestPath, this.manifest);
+  }
 }
 
-main();
+const postBuild = new PostBuild();
+postBuild.main();
