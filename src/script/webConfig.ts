2d702ef0f8a233f94639ee371ba3461b0e6e1b80
Richard Frost <richard@richardfrost.info>
2024-09-11 07:55:01 -0600

    :globe_with_meridians: Add defaultWordSubstitution

--- src/script/webConfig.ts
+++ src/script/webConfig.ts
@@ -350,6 +350,13 @@ export default class WebConfig extends Config {
 
   localizeDefaults() {
     if (!this._defaultsLoaded || !this._defaultsLoaded.length) return;
+
+    const translation = new Translation('common');
+
+    if (this._defaultsLoaded.includes('defaultSubstitution')) {
+      const options = { defaultValue: this.Class._defaults.defaultSubstitution };
+      this.defaultSubstitution = translation.t('common:defaultWordSubstitution', options);
+    }
   }
 
   // Order and remove `_` prefixed values
