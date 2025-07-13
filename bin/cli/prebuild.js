3e29c16317139dd7dd75dfa2d3a61c4820cc70ab
Richard Frost <richard@richardfrost.info>
2025-07-12 23:17:34 -0600

    💚 (build): Adjust prebuild cli output to match postbuild

--- bin/cli/prebuild.js
+++ bin/cli/prebuild.js
@@ -4,17 +4,15 @@ import Prebuild from '../lib/Prebuild.js';
 const prebuild = new Prebuild(process.argv);
 try {
   prebuild.run();
+  console.log(prebuild.successMessage('prebuild'));
 
-  // Output any warnings
   if (prebuild.hasWarnings) {
-    console.warn('\n⚠️  Warnings:');
+    console.warn(' ⚠️  Warnings:');
     prebuild.warnings.forEach((warning) => {
       console.warn(`  ${warning}`);
     });
     console.warn('');
   }
-
-  console.log(prebuild.successMessage('prebuild'));
 } catch (error) {
   console.error(prebuild.errorMessage('prebuild'), error.message);
   process.exit(1);
